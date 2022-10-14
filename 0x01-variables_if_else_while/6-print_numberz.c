c4ca2ec736bd4ab5641e14d38e2525b7f56cb467
Maa-mmy <dikecynthia22@gmail.com>
2022-10-14 21:04:35 +0100

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	int number; for (number = 0; number <= 10; ++number)
+	int number; for (number = '0'; number <= '10'; ++number)
 		putchar(number);
 	putchar('\n');
 	return (0);
