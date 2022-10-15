f0e70dd36b836a8c11a98385c9d2477774fcd27b
Maa-mmy <dikecynthia22@gmail.com>
2022-10-15 01:12:42 +0100

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	int num, alpha; for (num = 48; num <= 58; num++)
+	int num, alpha; for (num = '0'; num <= '9'; num++)
 		putchar(num);
 	for (alpha = 'a'; alpha <= 'f'; alpha++)
 		putchar(alpha);
