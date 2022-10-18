30578a89a895e00fedf66a8d1cf286fa1d954b79
Maa-mmy <dikecynthia22@gmail.com>
2022-10-18 18:18:44 +0100

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,13 @@
  */
 void print_alphabet_x10(void)
 {
-	int alpha, ten_times = 0; while (ten_times < 10)
+	int alpha = 'a';
+	int ten_times = 0; while (ten_times < 10)
 	{
-		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		for (alpha <= 'z')
 		{
-			putchar(alpha);
-			_putchar('\n'), ten_times++;
+			_putchar(alpha), alpha++;
 		}
+		_putchar('\n'), ten_times++;
 	}
 }
