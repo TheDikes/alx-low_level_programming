8dc500df4b5840d75ab395026484cdb916405023
Maa-mmy <dikecynthia22@gmail.com>
2022-10-19 15:43:55 +0100

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point for program
+ *
+ * Description - tests function that prints if integer is + or -
+ *
+ * Return: Always (0) success
+ */
+int main(void)
+{
+	int n;
+
+	n = 98;
+	positive_or_negative(n);
+	return (0);
+}
