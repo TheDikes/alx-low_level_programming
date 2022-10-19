2a1ecd90a5c303cf4c4f5de3bb8b1ca88bdf022a
Maa-mmy <dikecynthia22@gmail.com>
2022-10-19 04:16:43 +0100

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: should return 0
+ */
+int main(void)
+{
+	int a;
+	unsigned long int b, c, d total;
+
+	b = 1;
+	c = 2;
+	total = 0;
+
+	for (a = 1; a <= 33; a++)
+	{
+		if (b < 4000000 && (b % 2) == 0)
+		{
+			total = total + b;
+		}
+		d = b + c;
+		b = c;
+		c = d;
+	}
+	printf("%lu\n", total);
+	return (0);
+}
