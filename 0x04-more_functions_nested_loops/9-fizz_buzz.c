26a52c983fd97864dc3edeb6262979044ef28270
Maa-mmy <dikecynthia22@gmail.com>
2022-10-20 15:14:14 +0100

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the program fizzbuzz test
+ * Return: Always (0) success
+ */
+int main(void)
+{
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			_putchar("FizzBuzz\n");
+		}
+		else if (i % 5 == 0)
+		{
+			_putchar("Buzz\n");
+		}
+		else if (i % 3 == 0)
+		{
+			_putchar("Fizz\n");
+			if (i != 100)
+			{
+				_putchar(' ');
+			}
+		}
+		else
+		{
+			_putchar("%d\n", i);
+		}
+	}
+}
