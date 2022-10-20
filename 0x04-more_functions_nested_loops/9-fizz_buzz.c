e13fd18f93feaa2c2a638b5104f371430d28d4ac
Maa-mmy <dikecynthia22@gmail.com>
2022-10-20 16:55:25 +0100

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,10 +10,10 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf(" Fizz");
-		} else if (i % 5 == 0)
+		} else if (i % 5 == 0 && 3 != 0)
 		{
 			printf(" Buzz");
 		} else if (i % 5 == 0 && i % 3 == 0)
