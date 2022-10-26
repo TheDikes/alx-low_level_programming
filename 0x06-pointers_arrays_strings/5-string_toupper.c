5a5c20beefd8ff9af2ded51cc576eee31e2ec8a6
Maa-mmy <dikecynthia22@gmail.com>
2022-10-27 00:05:36 +0100

    I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,8 +12,10 @@ char *string_toupper(char *s)
 	int i;
 
 	i = 0; while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
 		i++;
-	if (s[i] >= 'a' && s[i] <= 'z')
-		s[i] = s[i] - 32;
+	}
 	return (s);
 }
