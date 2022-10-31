cad3aa3c59f1b156a94276726ec787626bcd3c0a
Maa-mmy <dikecynthia22@gmail.com>
2022-10-31 16:06:51 +0100

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,7 +14,7 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; *s != '\0'; i++)
 	{
-		for (j = 0; *accept != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
